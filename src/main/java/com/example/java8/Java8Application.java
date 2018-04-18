4a88d41fa24d2a7ed4f6a05c47c85dc30e604ae9
chengchen168168 <chengchen168168@163.com>
2018-04-18 17:03:40 +0800

    Update Java8Application.java

--- src/main/java/com/example/java8/Java8Application.java
+++ src/main/java/com/example/java8/Java8Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Java8Application {
 
 	public static void main(String[] args) {
+		//。。。
 		SpringApplication.run(Java8Application.class, args);
 	}
 }
